6bac90a3d384f5a2e4129c6a4c6d11105a1532c9
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-04-09 19:12:28 +0300

    add 1 task and create gitognore file

---
+++ async_1/2_task.py
@@ -0,0 +1,27 @@
+import asyncio
+import time
+
+
+async def interviews(*args):
+    async def process_interview(name, time_prof1, time_def1, time_prof2, time_def2):
+        print(f"{name} started the 1 task.")
+        await asyncio.sleep(time_prof1 / 100)
+        print(f"{name} moved on to the defense of the 1 task.")
+        await asyncio.sleep(time_def1 / 100)
+        print(F"{name} completed the 1 task.")
+
+        print(f"{name} is resting.")
+        await asyncio.sleep(5 / 100)
+
+        print(f"{name} started the 2 task.")
+        await asyncio.sleep(time_prof2 / 100)
+        print(f"{name} moved on to the defense of the 2 task.")
+        await asyncio.sleep(time_def2 / 100)
+        print(F"{name} completed the 2 task.")
+
+    tasks = []
+    for name, time_prof1, time_def1, time_prof2, time_def2 in args:
+        tasks.append(asyncio.create_task(process_interview(
+            name, time_prof1, time_def1, time_prof2, time_def2)))
+    await asyncio.gather(*tasks)
+    
